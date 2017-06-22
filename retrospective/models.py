9109972ea419ebe1bba03e2a6f1611a272266ef6
Dr-Lord <Lord.Fletcher@outlook.com>
2017-06-22 11:53:51 +0100

    Models update; merged Member and TeamUser into Coder

--- retrospective/models.py
+++ retrospective/models.py
@@ -1,20 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Member(models.Model):
-	user = models.OneToOneField(User)
-	role = models.CharField(max_length=255)
-	
-	def __unicode__(self):
-		return self.user.username
-
 class Team(models.Model):
     name = models.CharField(max_length=255)
 
-class TeamUser(models.Model):
-	team = models.ForeignKey(Team,on_delete=models.CASCADE)
-	member = models.ForeignKey(Member,on_delete=models.CASCADE)
+class Coder(models.Model):
+	user = models.OneToOneField(User)
+	team = models.ForeignKey(Team, on_delete=models.CASCADE)
 	manager = models.BooleanField()
+
+	def __unicode__(self):
+		return self.user.username
 
 class Retrospective(models.Model):
     name = models.CharField(max_length=255)
@@ -25,4 +21,5 @@ class Item(models.Model):
     name = models.CharField(max_length=255)
     itemType = models.CharField(max_length=255)
     retro = models.ForeignKey(Retrospective, on_delete=models.CASCADE)
-    creator = models.ForeignKey(Member, on_delete=models.CASCADE)
+    creator = models.ForeignKey(Coder, on_delete=models.CASCADE)
+    creationDate = models.DateField()

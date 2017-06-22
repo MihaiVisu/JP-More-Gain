2d745f8a4847c59667e1c5bc66ad6c8ee1fcafc7
Dr-Lord <Lord.Fletcher@outlook.com>
2017-06-22 11:31:46 +0100

    First models implemented

--- retrospective/models.py
+++ retrospective/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Retrospective(models.Model):
+    name = models.CharField()
+    type = models.CharField()
+    owner = models.ForeignKey(Team, on_delete=models.CASCADE)
+
+class Item(models.Model):
+    name = models.CharField()
+    type = models.CharField()
+    retro = models.ForeignKey(Retrospective, on_delete=models.CASCADE)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class User(models.Model):
+    fst_name = CharField()
+    last_name = CharField()
+    role = CharField()
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+
+class Team(models.Model):
+    name = CharFIeld()
+    manager = models.ForeignKey(User, on_delete=models.CASCADE)
+

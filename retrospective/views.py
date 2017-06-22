a2b4d2358c0d8e0989d834c293dba6d0d9bfb2e3
Mihai <mihaivisuian@gmail.com>
2017-06-22 12:05:01 +0100

    add login and logout views

--- retrospective/views.py
+++ retrospective/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth import login, authenticate
 
-# Create your views here.
+
+def login_view(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username, password)
+    if user:
+        return 'SUCCESS'
+    else:
+        return 'INCORRECT LOGIN'
+
+
+def logout_view(request):
+    logout(request)
